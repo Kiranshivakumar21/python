8911e1ed540a675e43e4d465a431da0f8478515d
kiranshivakumar21 <kiranshivakumar21@gmail.com>
2025-10-25 12:53:06 +0530

    class&object

---
+++ task-25.10.25/democlass.py
@@ -0,0 +1,11 @@
+class gurutheatre:
+    def kantara(self):
+        print("enjoying kantara movie")
+    def dude(self):
+        print("enjoy the dude movie")
+
+v = gurutheatre()
+k = gurutheatre()
+
+v.kantara()
+k.dude()
